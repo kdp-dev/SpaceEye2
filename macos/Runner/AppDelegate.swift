ce0e9284414de2c2359373eed9c443622f02523e
KYDronePilot <kydronepilotdev@gmail.com>
2021-08-07 15:31:10 -0400

    Added native code to use macOS NSPopover

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,37 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    let popover = NSPopover()
+
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return false
+    }
+
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if let button = statusItem.button {
+            button.image = NSImage(named: NSImage.Name("AppIcon"))
+            button.action = #selector(togglePopover(_:))
+        }
+        popover.behavior = .transient
+        popover.contentViewController = FlutterViewController.init()
+    }
+
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+    }
 }
